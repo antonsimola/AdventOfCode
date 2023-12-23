1a648956df9bab75ee074c5de2e6ab94893d98f9
Anton <anton.simola@gmail.com>
2023-12-23 14:13:28 +0200

    wip 12

--- AdventOfCode2023/Day12.cs
+++ AdventOfCode2023/Day12.cs
@@ -1,146 +1,156 @@
+using System.Runtime.InteropServices.Marshalling;
 using System.Text;
+using System.Text.RegularExpressions;
+using MathNet.Numerics;
 
 namespace AdventOfCode2023;
 
 public class Day12 : BaseDay
 {
-    private Dictionary<string, ICollection<IList<char>>> Cache = new ();
-    
+    private Dictionary<string, ICollection<IList<char>>> Cache = new();
+
     //TODO brute force, takes years on full input. Memoization? Split into unknown parts and known parts
     public override void Run()
     {
-        // var input = Input;
-        var input = AllTestInputs[1];
+        //https://old.reddit.com/r/adventofcode/comments/18ghux0/2023_day_12_no_idea_how_to_start_with_this_puzzle/kd0npmi/
+        var input = Input;
+        // var input = AllTestInputs[1];
         var grandSum = 0;
         var progress = 0;
+        var cache = new Dictionary<string, IList<string>>();
+
+        var reg = new Regex("([?]*)");
+        
+        for (var i = 1; i < 15; i++)
+        {
+            Console.WriteLine(i);
+            Generate(new string('?', i), cache);
+        }
+
+        var globalmax = 0;
         foreach (var line in input)
         {
             var split = line.Split(" ");
             // var records = split[0];
             var records = string.Join("?", Enumerable.Range(0, 5).Select(i => split[0]));
             var checks = split[1].ParseIntList(",");
-            checks = Enumerable.Range(0, 5).Select(i => checks).Aggregate((a, b) => a.Concat(b).ToList());
+            // checks = Enumerable.Range(0, 5).Select(i => checks).Aggregate((a, b) => a.Concat(b).ToList());
 
-
-            var grouped = GroupIntoKnownsAndUnknowns(records);
-
-            var checksCount = checks.Sum();
-
-            var allRecords = Generate(records).ToList();
-
-
-            Console.WriteLine((double)progress / input.Length);
+            // var max = 0;
+            // foreach (Match match in reg.Matches(records))
+            // {
+            //         max = Math.Max(   match.Groups[1].Length, max);
+            // }
+            //
+            // Console.WriteLine(max);
+            // globalmax = Math.Max(globalmax, max);
+            
+            
+            
+            var checksQ = new Queue<int>(checks);
+            
+            var ways = records.Split(".");
 
             var sum = 0;
-            // Console.WriteLine("records : " + records);
-            foreach (var r in allRecords)
+             
+            foreach (var w in ways)
             {
-                var checksQ = new Queue<int>(checks);
-                var ok = true;
-                for (var i = 0; i < r.Length; i++)
-                {
-                    var c = r[i];
-                    if (c == '.')
-                    {
-                        continue;
-                    }
-
-                    if (c == '#')
-                    {
-                        if (!checksQ.TryDequeue(out var checking))
-                        {
-                            ok = false;
-                            break;
-                        }
-
-                        var brokenCount = 0;
-                        while (i < r.Length && r[i] == '#')
-                        {
-                            brokenCount++;
-                            i++;
-                        }
-
-                        if (brokenCount != checking)
-                        {
-                            ok = false;
-                        }
-                    }
-                }
-
-                if (ok && checksQ.Count == 0)
-                {
-                    // Console.WriteLine("record OK : " + r);
-                    sum += 1;
-                }
+                 Generate(w, cache);
+                 
             }
-
             grandSum += sum;
-            Console.WriteLine("Done this round");
-
-            progress++;
         }
 
         Console.WriteLine("--------");
+        Console.WriteLine(globalmax);
         Console.WriteLine(grandSum);
     }
 
-    private List<string> GroupIntoKnownsAndUnknowns(string records)
-    {
-        var parts = new List<string>();
-        var currentPart = "";
-        char prev = records[0];
-        foreach (var c in records.Skip(1))
-        {
-            if (prev == '?' && c != '?')
-            {
-                parts.Add(currentPart);
-                currentPart = "";
-            }
-            else if (prev != '?' && c == '?')
-            {
-                parts.Add(currentPart);
-                currentPart = "";
-            }
 
-            prev = c;
-            currentPart += c;
+    public int RecCheck(string s, Queue<int> checksQ, Dictionary<string, IList<string>> cache)
+    {
+        if (s.Length == 0 && checksQ.Count == 0)
+        {
+            Console.WriteLine("Counting 1");
+            return 1;
         }
 
+        if (s.Length == 0 && checksQ.Count != 0) return 0;
 
-        return parts;
+        var sum = 0;
+        if (s[0] == '.')
+        {
+            sum += RecCheck(s[1..], checksQ, cache);
+        }
+        else if (s[0] == '?')
+        {
+            sum += RecCheck("#" + s[1..], checksQ, cache);
+            sum += RecCheck("." + s[1..], checksQ, cache);
+        }
+
+        else if (s[0] == '#')
+        {
+            
+            var i = 0;
+            while (i < s.Length && s[i] == '#')
+            {
+                i++;
+            }
+            if (i == s.Length || (s[i] == '.' && i == checksQ.Peek()))
+            {
+                var newQ = new Queue<int>(checksQ);
+                var check = newQ.Dequeue();
+                sum += RecCheck(s[i..], newQ, cache);
+            }
+            else
+            {
+                
+                sum += RecCheck(s[i..], checksQ, cache);
+            }
+        }
+
+        return sum;
     }
 
 
-    public IEnumerable<string> Generate(string s)
+    public IList<string> Generate(string s, Dictionary<string, IList<string>> memo)
     {
-        var jokerLocations = new List<int>();
-        var i = 0;
-
-        foreach (var card in s)
+        // generate from bottom to top, only ? , ??, ???, ???, then merge those into strings?
+        // divide and conquer?
+        if (memo.TryGetValue(s, out var cached))
         {
-            if (card == '?')
-            {
-                jokerLocations.Add(i);
-            }
-
-            i++;
+            return cached;
         }
 
-        char[] chars = ['#', '.'];
-        var res = new List<string>();
-        var combines = chars.CombineWithRepetitions(jokerLocations.Count, Cache);
+        if (s == "?") return memo.GetOrAdd("?", e => ["#", "."]);
+        if (s == "#") return memo.GetOrAdd("#", e => ["#"]);
+        if (s == ".") return memo.GetOrAdd(".", e => ["."]);
 
-        foreach (var set in combines)
+        var head = s[0];
+        var tail = s.Substring(1);
+
+        var res = Generate(tail, memo);
+
+        var appended = new List<string>();
+        if (head == '?')
         {
-            var h = new StringBuilder(s);
-            var j = 0;
-            foreach (var loc in jokerLocations)
-            {
-                h[loc] = set[j++];
-            }
-
-            res.Add(h.ToString());
+            appended.AddRange(res.Select(str => "." + str));
+            appended.AddRange(res.Select(str => "#" + str));
         }
+        else
+        {
+            appended.AddRange(res.Select(str => head + str));
+        }
+
+
+        memo[s] = appended;
+        return appended;
+    }
+
+
+    public IList<string> Generate(string s)
+    {
+        var res = Generate(s, new Dictionary<string, IList<string>>());
 
         return res;
     }
